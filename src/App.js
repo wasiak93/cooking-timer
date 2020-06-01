319600752011a7bd673dfe13f819bc5a4c4c479d
wasiak93 <kacperjacekwasiak@gmail.com>
2020-06-01 16:31:23 +0200

    add timer`
    git add .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [start, setStart] = useState(false);
+  let [timerValue, setTimerValue] = useState(120);
+
+  const changeTime = () => {
+    setTimerValue(--timerValue);
+  };
+  const hanndlerTimer = () => {
+    setInterval(() => changeTime(), 1000);
+    setStart(true);
+  };
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="app">
+      <form action="" className="form">
+        form
+      </form>
+
+      <main className="main">
+        {start ? null : <button onClick={() => hanndlerTimer()}>Start</button>}
+        {timerValue}
+        {timerValue <= 0 ? <div>time is over</div> : null}
+      </main>
     </div>
   );
-}
+};
 
 export default App;
