5e023c4e5cb881da7779b4623d2b090328f3c710
wasiak93 <kacperjacekwasiak@gmail.com>
2020-06-02 16:42:55 +0200

    add condition for showin minutes,seconds,cseconds

--- src/Main/Timer/Clock/Clock.js
+++ src/Main/Timer/Clock/Clock.js
@@ -5,16 +5,20 @@ const Clock = ({ timerValue }) => {
   // const hours = Math.floor(timerValue / (60 * 60))
   //   .toString()
   //   .padStart(2, 0);
-
-  const minutes = Math.floor((timerValue / 60 / 100) % 60)
-    .toString()
-    .padStart(2, 0);
-  const seconds = Math.floor((timerValue / 100) % 60)
-    .toString()
-    .padStart(2, 0);
-  const cseconds = Math.floor(timerValue % 100)
-    .toString()
-    .padStart(2, 0);
+  let minutes = 0;
+  let seconds = 0;
+  let cseconds = 0;
+  if (timerValue > 0) {
+    minutes = Math.floor((timerValue / 60 / 100) % 60)
+      .toString()
+      .padStart(2, 0);
+    seconds = Math.floor((timerValue / 100) % 60)
+      .toString()
+      .padStart(2, 0);
+    cseconds = Math.floor(timerValue % 100)
+      .toString()
+      .padStart(2, 0);
+  }
   return (
     <div className="clock">
       {/* <div className="clock__item">{hours}</div> */}
