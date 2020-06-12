4a7720f5f2422dcfefda9d5f1eca476ca9f8b837
wasiak93 <kacperjacekwasiak@gmail.com>
2020-06-12 15:40:24 +0200

    Updates

---
+++ precache-manifest.598d593547be8dd7a2d125a0e9af31b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480b017b7dccb64527fcc3f8c1e98593",
+    "url": "/cooking-timer/index.html"
+  },
+  {
+    "revision": "8f840cbcfc5e889c102b",
+    "url": "/cooking-timer/static/css/main.49057820.chunk.css"
+  },
+  {
+    "revision": "e7e57f080fcdd54d8cb1",
+    "url": "/cooking-timer/static/js/2.37074170.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cooking-timer/static/js/2.37074170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f840cbcfc5e889c102b",
+    "url": "/cooking-timer/static/js/main.eee4b7a0.chunk.js"
+  },
+  {
+    "revision": "7994516f11a90b5fdb97",
+    "url": "/cooking-timer/static/js/runtime-main.d9b4a05f.js"
+  },
+  {
+    "revision": "fe098666b681ec5d0b3df6fa57d8a65e",
+    "url": "/cooking-timer/static/media/alarm.fe098666.mp3"
+  }
+]);
